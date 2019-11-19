995013f88e49a130f27574a4b8930e2708834366
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-19 06:51:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,6 @@ class Person
     attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(elements)
-    elements.each { |key, value| self.send(key, value) }
-    #self.send("#{key}=", value) }
+    elements.each { |key, value| self.send("#{key}=", value) }
   end
 end
